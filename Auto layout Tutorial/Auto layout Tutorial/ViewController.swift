9547e7291c0f27fb7b6b6b4ede254439d81c7729
Blacklord100 <mithuran100@gmail.com>
2020-11-14 18:03:42 +0100

    first commit with HelloWorld and Auto Layout

---
+++ Auto layout Tutorial/Auto layout Tutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto layout Tutorial
+//
+//  Created by Mithuran Gajendran on 14/11/2020.
+//  Copyright © 2020 Mithuran Gajendran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
