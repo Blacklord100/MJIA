54704d6fdfea8eefb239842f10e3466e10cc32b1
Blacklord100 <mithuran100@gmail.com>
2020-12-14 16:58:47 +0100

    up

---
+++ 100 Days of SwiftUI/Day 1/MyPlayground.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+var str = """
+a
+haha I love
+hcoco
+"""
+var myInt = 1.0
+var myDouble = 3.9
+var lol = myInt + myDouble
+
+
+var city = 1
+var message = "94 c'est la champions league \(city)"
+
+let constVar = 9
+
+var percentage: Double = 99
+var name: String
+
+var myArray = [1,2]
+myArray[1]
+var mySet = Set([1,2,2])
+var myTuple = (name:1,nb:2)
+myTuple.nb
+
+var myDict = ["fr":1,"eng":2]
+myDict["lol"]
